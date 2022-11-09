08c80e93a227666c0f60d1be1e7b422ea882a521
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2022-11-09 11:47:30 +0100

    Do some additional exercises

---
+++ client/tests/fixtures.ts
@@ -0,0 +1,49 @@
+import { expect, Page } from '@playwright/test';
+
+export const createSong = async (page: Page) => {
+  await page.goto('http://localhost:8080/');
+
+  await page.getByRole('link', { name: 'add' }).click();
+  await expect(page).toHaveURL('http://localhost:8080/#/songs/create');
+
+  await page.getByLabel('Title').click();
+
+  await page.getByLabel('Title').fill('Sample Title');
+
+  await page.getByLabel('Title').press('Tab');
+
+  await page.getByLabel('Artist').fill('Sample Artist');
+
+  await page.getByLabel('Artist').press('Tab');
+
+  await page.getByLabel('Genre').fill('Sample Genre');
+
+  await page.getByLabel('Genre').press('Tab');
+
+  await page.getByRole('textbox', { name: 'Album*' }).fill('Sample Album');
+
+  await page.getByRole('textbox', { name: 'Album*' }).press('Tab');
+
+  await page.getByLabel('Album Image Url').fill('localhost:8080');
+
+  await page.getByLabel('Album Image Url').press('Tab');
+
+  await page.getByLabel('YouTube ID').fill('SampleYouTubeID');
+
+  await page.getByLabel('YouTube ID').press('Tab');
+
+  await page.getByLabel('Tab').fill('Sample Tab');
+
+  await page.getByLabel('Tab').press('Tab');
+
+  await page.getByLabel('Lyrics').fill('Sample Lyrics');
+
+  await page.getByRole('button', { name: 'Create Song' }).click();
+  await expect(page).toHaveURL('http://localhost:8080/#/songs');
+
+  await page.getByText('Sample Title').click();
+};
+
+export const clean = async (page: Page) => {
+  await page.goto('http://localhost:8081/reset');
+}
--- client/tests/navigation.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
-
-  await page.getByRole('textbox', { name: 'Search by song title, artist, album, or genre' }).fill('nirvana');
-
-  await expect(page).toHaveURL('http://localhost:8080/#/songs?search=nirvana');
-});

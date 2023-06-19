92b76fbd5736f8b4c4e92f9c907fe21dd7b02ab5
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-06-19 16:05:30 +0200

    fix visual regression test

--- client/tests/visual-regression.test.ts
+++ client/tests/visual-regression.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Navigate to homepage and compare visual', async ({ page }) => {
   await page.goto('http://localhost:8080');
-  await expect(page).toHaveScreenshot({ mask: [page.getByAltText('random-movie-poster')] });
+  await expect(page).toHaveScreenshot({ mask: [page.getByAltText('random-image')] });
 });

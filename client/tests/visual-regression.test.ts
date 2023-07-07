fe5eb5bdd99dc381c6d9a6aa8731e09f9e27e527
Riccardo Corradin <riccardo.corradin@live.com>
2023-07-07 11:19:20 +0200

    fix: a nice qxperts green color

--- client/tests/visual-regression.test.ts
+++ client/tests/visual-regression.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Navigate to homepage and compare visual', async ({ page }) => {
   await page.goto('http://localhost:8080');
-  await expect(page).toHaveScreenshot({ mask: [page.getByAltText('random-image')] });
+  await expect(page).toHaveScreenshot({ mask: [page.getByAltText('random-image')], maskColor: '#009863'});
 });

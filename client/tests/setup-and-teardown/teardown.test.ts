b2feac84bc690204ef142f32d4b2e1f8c19577be
Riccardo Corradin <riccardo.corradin@live.com>
2023-09-12 15:11:59 +0200

    fix failing test

--- client/tests/setup-and-teardown/teardown.test.ts
+++ client/tests/setup-and-teardown/teardown.test.ts
@@ -3,6 +3,8 @@ import { test as teardown } from '@playwright/test';
 // teardown.use({launchOptions: {slowMo: 2000}});
 const storageState = 'tests/storage-state/storageState.json';
 
+teardown.use({storageState: storageState});
+
 teardown('Log out', async ({ page }) => {
     await page.goto('http://localhost:8080/#/songs');
     await page.getByText('Log Out').click();

6f46623d03446c3035513d88808240bfa0b46621
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-10-20 15:35:55 +0200

    fix: some refactoring

---
+++ client/tests/java/src/main/java/org/tests/ExampleTest.java
@@ -0,0 +1,14 @@
+package org.tests;
+
+import com.microsoft.playwright.*;
+
+public class ExampleTest {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium()
+                    .launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+        }
+    }
+}

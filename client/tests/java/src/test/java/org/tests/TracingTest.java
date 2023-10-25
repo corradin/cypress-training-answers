471bcb028087a1c26ccec4fbfef30b7e184ac6e4
James Lloyd <jlloyd@qxperts.io>
2023-10-25 16:49:29 +0200

    Add tracing test

---
+++ client/tests/java/src/test/java/org/tests/TracingTest.java
@@ -0,0 +1,35 @@
+package org.tests;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+
+import java.nio.file.Paths;
+import org.testng.annotations.Test;
+// import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class TracingTest {
+
+    @Test
+    public void tracingTest() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            BrowserContext context = browser.newContext();
+
+            // Start tracing before creating / navigating a page.
+            context.tracing().start(new Tracing.StartOptions()
+                    .setScreenshots(true)
+                    .setSnapshots(true)
+                    .setSources(true));
+
+            Page page = context.newPage();
+            page.navigate("http://playwright.dev");
+            page.getByText("Getting Started").click();
+
+            PlaywrightAssertions.assertThat(page).hasURL("https://playwright.dev/docs/intro");
+
+            // Stop tracing and export it into a zip archive.
+            context.tracing().stop(new Tracing.StopOptions()
+                    .setPath(Paths.get("trace.zip")));
+        }
+    }
+}

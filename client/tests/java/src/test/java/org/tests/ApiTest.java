c42433b1f8b8bc10e86cbb01a622d9ae3192ce5b
James Lloyd <jlloyd@qxperts.io>
2023-10-25 15:54:52 +0200

    Add API test

---
+++ client/tests/java/src/test/java/org/tests/ApiTest.java
@@ -0,0 +1,87 @@
+package org.tests;
+
+import com.microsoft.playwright.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.testng.Assert;
+
+public class ApiTest {
+
+    // Shared between all tests in this class.
+    private Playwright playwright;
+    private APIRequestContext request;
+
+    void createPlaywright() {
+        playwright = Playwright.create();
+    }
+
+    void createAPIRequestContext() {
+        Map<String, String> headers = new HashMap<>();
+
+        request = playwright.request().newContext(new APIRequest.NewContextOptions()
+                // All requests we send go to this API endpoint.
+                .setBaseURL("http://localhost:8081")
+                .setExtraHTTPHeaders(headers));
+    }
+
+    @BeforeSuite
+    void beforeAll() {
+        createPlaywright();
+        createAPIRequestContext();
+    }
+
+    void disposeAPIRequestContext() {
+        if (request != null) {
+            request.dispose();
+            request = null;
+        }
+    }
+
+    void closePlaywright() {
+        if (playwright != null) {
+            playwright.close();
+            playwright = null;
+        }
+    }
+
+    @AfterSuite
+    void afterAll() {
+        disposeAPIRequestContext();
+        closePlaywright();
+    }
+
+    @Test
+    void retrieveTheSongsFromTheAPI() throws JSONException { 
+        APIResponse issues = request.get("/songs");
+        Assert.assertTrue(issues.ok());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 1);
+        map.put("title", "Nevermind");
+        map.put("artist", "Nirvana");
+        map.put("genre", "Alternative Rock");
+        map.put("album", "Nevermind");
+        map.put("albumImageUrl", "https://is3-ssl.mzstatic.com/image/thumb/Features/d0/cc/62/dj.nanioukp.jpg/268x0w.jpg");
+        map.put("youtubeId", "m-ofL_3EZyE");
+        map.put("lyrics", "");
+        map.put("tab", "");
+        map.put("createdAt", "2018-02-13T12:56:24.432Z");
+
+        JSONObject expected = new JSONObject(map);
+
+        JSONArray actual = new JSONArray(issues.text());
+
+        JSONAssert.assertEquals(expected, actual.getJSONObject(0), JSONCompareMode.LENIENT);
+    }
+
+}

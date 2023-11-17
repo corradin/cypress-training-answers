7d3662644cdb90102758662dac5fb490451fd049
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-11-17 09:44:58 +0100

    fix(O): DRY using codesandbox

---
+++ client/tests/java/src/test/java/org/tests/utils/Constants.java
@@ -0,0 +1,6 @@
+package org.tests.utils;
+
+public class Constants {
+    public static String SERVER_URL = "https://tgk47x-8081.csb.app";
+    public static String CLIENT_URL = "http://localhost:8080";
+}

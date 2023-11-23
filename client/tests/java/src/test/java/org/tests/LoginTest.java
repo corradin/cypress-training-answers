6ba42d0aacc0377d20dd1af085a0f8742f186e51
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-11-23 15:44:41 +0100

    headless on

--- client/tests/java/src/test/java/org/tests/LoginTest.java
+++ client/tests/java/src/test/java/org/tests/LoginTest.java
@@ -27,7 +27,7 @@ public class LoginTest {
     @BeforeClass
     void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
     }
 
     @AfterClass

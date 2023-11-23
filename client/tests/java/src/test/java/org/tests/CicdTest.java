bd2d7c4af8677c3e9dde2f759fefb75f534aa2ca
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-11-23 15:59:11 +0100

    cicd specific test

---
+++ client/tests/java/src/test/java/org/tests/CicdTest.java
@@ -0,0 +1,51 @@
+package org.tests;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.tests.utils.Constants;
+
+public class CicdTest {
+    Playwright playwright;
+    Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeClass
+    void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @AfterClass
+    void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeMethod
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterMethod
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    public void nirvanaNameTest() {
+        page.navigate(Constants.CLIENT_URL);
+        page.getByText("Nirvana").locator("..")
+                .getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName("VIEW")).click();
+
+        assertThat(page.getByText("Nirvana")).isVisible();
+    }
+}

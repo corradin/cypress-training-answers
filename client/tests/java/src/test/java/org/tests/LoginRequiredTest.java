df9c61369743e51f478c2128a42f25adf8403ad2
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-11-23 13:56:00 +0100

    fix: removed headless

--- client/tests/java/src/test/java/org/tests/LoginRequiredTest.java
+++ client/tests/java/src/test/java/org/tests/LoginRequiredTest.java
@@ -24,7 +24,7 @@ public class LoginRequiredTest {
     @BeforeClass
     void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
     }
 
     @AfterClass

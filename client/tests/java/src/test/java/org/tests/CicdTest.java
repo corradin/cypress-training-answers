8ac78cdc3d367d8ed015ac19274d23258c3e623c
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2023-11-23 16:00:30 +0100

    removed headed mode

--- client/tests/java/src/test/java/org/tests/CicdTest.java
+++ client/tests/java/src/test/java/org/tests/CicdTest.java
@@ -21,7 +21,7 @@ public class CicdTest {
     @BeforeClass
     void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
     }
 
     @AfterClass

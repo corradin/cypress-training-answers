f84c93d703ec752010db72a746d057377033f583
Riccardo <riccardo.corradin@live.com>
2024-04-19 19:35:05 +0200

    updated tests for new button

--- client/tests/parallel.test.ts
+++ client/tests/parallel.test.ts
@@ -5,7 +5,7 @@ test.describe.configure({mode: 'parallel'});
 test('Check Nirvana page contains Nirvana', async ({ page }) => {
   await page.goto('http://localhost:8080/');
 
-  await page.getByText('Nirvana').locator('..').getByRole('link', { name: 'VIEW' }).click();
+  await page.getByText('Nirvana').locator('..').getByRole('button', { name: 'View', exact: true }).click();
 
   await expect(page).toHaveURL('http://localhost:8080/#/songs/1');
   await page.getByText('Nirvana').click();
@@ -14,7 +14,7 @@ test('Check Nirvana page contains Nirvana', async ({ page }) => {
 test('Check Cypress Hill page contains Cypress Hill', async ({ page }) => {
     await page.goto('http://localhost:8080/');
 
-    await page.getByText('Cypress Hill').locator('..').getByRole('link', { name: 'VIEW' }).click();
+    await page.getByText('Cypress Hill').locator('..').getByRole('button', { name: 'View', exact: true }).click();
     
     await expect(page).toHaveURL('http://localhost:8080/#/songs/4');
     await page.getByText('Cypress Hill').click();

993c6b2629879e73853df6073b77778362dc12b2
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2025-04-02 16:37:40 +0200

    fix: local server

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,14 +1,15 @@
-import axios from 'axios'
-import store from '@/store/store'
+import axios from 'axios';
+import store from '@/store/store';
 
 //For local development use: http://localhost:8081/
-const baseUrl = 'https://tgk47x-8081.csb.app/';
+// const baseUrl = 'https://tgk47x-8081.csb.app/';
+const baseUrl = 'http://localhost:8081/';
 
 export default () => {
   return axios.create({
     baseURL: baseUrl,
     headers: {
-      Authorization: `Bearer ${store.state.token}`
-    }
-  })
-}
+      Authorization: `Bearer ${store.state.token}`,
+    },
+  });
+};

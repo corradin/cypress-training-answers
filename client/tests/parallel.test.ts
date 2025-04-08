4fccc179db56a21590ac0543b8bdc9d8fa6d19fe
Riccardo Corradin <riccardo.corradin@debijenkorf.nl>
2025-04-08 13:39:02 +0200

    fix: parallel mode

--- client/tests/parallel.test.ts
+++ client/tests/parallel.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.configure({mode: 'parallel'});
+test.describe.configure({ mode: 'parallel' });
 
 test('Check Nirvana page contains Nirvana', async ({ page }) => {
   await page.goto('http://localhost:8080/');
@@ -12,10 +12,10 @@ test('Check Nirvana page contains Nirvana', async ({ page }) => {
 });
 
 test('Check Cypress Hill page contains Cypress Hill', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+  await page.goto('http://localhost:8080/');
 
-    await page.getByText('Cypress Hill').locator('..').getByRole('button', { name: 'View', exact: true }).click();
-    
-    await expect(page).toHaveURL('http://localhost:8080/#/songs/4');
-    await page.getByText('Cypress Hill').click();
+  await page.getByText('Cypress Hill').locator('..').getByRole('button', { name: 'View', exact: true }).click();
+
+  await expect(page).toHaveURL('http://localhost:8080/#/songs/4');
+  await page.getByText('Cypress Hill').click();
 });
